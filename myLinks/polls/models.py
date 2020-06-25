fe06c5af3268929e813d64ceace1fed45d7759e7
kaanatesel <kaanatesel99@gmail.com>
2020-06-25 03:12:52 +0300

    database connection and user and link tables createded

--- myLinks/polls/models.py
+++ myLinks/polls/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    nickname = models.CharField(max_length=250,unique=True)
+    firstname = models.CharField(max_length=250)
+    lastname = models.CharField(max_length=250)
+    email = models.EmailField()
+    description = models.TextField()
+
+    def __str__(self):
+        return self.nickname
+
+class Link(models.Model):
+    name = models.CharField(max_length=250)
+    click_count = models.IntegerField(default=0)
+    active = models.BooleanField(default=True)
+    color_code = models.CharField(max_length=50,default="#ffffff")
+    user_id = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.name
